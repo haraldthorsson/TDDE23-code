a0911311e549fcdbe372b2bc9029b69c62a55212
Harald Thorsson <49102828+haraldthorsson@users.noreply.github.com>
2024-09-03 15:31:00 +0200

    Add files via upload

---
+++ delbar_med_tre.py
@@ -0,0 +1,6 @@
+def delbar_med_tre(input_number):
+    print(str(input_number) + " divided by 3 equals " + str((input_number/3)))
+
+delbar_med_tre(1)
+delbar_med_tre(6)
+delbar_med_tre(4)
---
+++ max2_och_max3.py
@@ -0,0 +1,5 @@
+def max2(operator_one, operator_two):
+    if(operator_one > operator_two):
+        print(operator_one)
+    else:
+        print(operator_two)
